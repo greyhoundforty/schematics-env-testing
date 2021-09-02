2d6aff5fb5e3acecd195bcddc2cf26f4b15f34cd
greyhoundforty <yantiffany@fastmail.com>
2021-09-02 14:07:03 -0500

    yep
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 locals {
-  time = formatdate("DDhhmm", timestamp())
+  time   = formatdate("DDhhmm", timestamp())
+  region = lookup(data.external.env.result, "TF_VAR_SCHEMATICSLOCATION", "")
+  geo    = substr(local.region, 0, 2)
+  schematics_ssh_access_map = {
+    us = ["169.61.99.176/28", "169.62.1.224/28", "169.63.150.144/28", "169.63.173.208/28", "169.47.104.160/28", "169.60.172.144/28", "169.63.254.64/28"],
+    eu = ["158.175.90.16/28", "158.175.138.176/28", "141.125.79.160/28", "158.176.134.80/28", "158.177.210.176/28", "158.177.216.144/28", "161.156.138.80/28", "149.81.135.64/28"]
+  }
+  schematics_ssh_access = lookup(local.schematics_ssh_access_map, local.geo, ["0.0.0.0/0"])
 }
 
 
--- outputs.tf
+++ outputs.tf
@@ -2,22 +2,22 @@
 #   value = jsonencode(data.external.env)
 # }
 
-output "resource_group" {
-  value = data.ibm_resource_group.project
-}
+# output "resource_group" {
+#   value = data.ibm_resource_group.project
+# }
 
-output "vpc_info" {
-  value = data.ibm_is_vpc.project
-}
+# output "vpc_info" {
+#   value = data.ibm_is_vpc.project
+# }
 
 output "tgw_info" {
   value = data.ibm_tg_gateway.project
 }
 
 
-output "region_info" {
-  value = data.ibm_is_region.mzr
-}
+# output "region_info" {
+#   value = data.ibm_is_region.mzr
+# }
 
 output "zones" {
   value = data.ibm_is_zones.mzr
@@ -26,3 +26,9 @@ output "zones" {
 output "schematics_location" {
   value = lookup(data.external.env.result, "TF_VAR_SCHEMATICSLOCATION", "")
 }
+
+output "schematics_ssh_access_ips" {
+  value = local.schematics_ssh_access
+}
+
+
