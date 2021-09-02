7ba6c01618d80f58cc52e79ae637b45987fb8672
greyhoundforty <yantiffany@fastmail.com>
2021-09-02 16:04:47 -0500

    Output schematics details to file
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ locals {
   region = lookup(data.external.env.result, "TF_VAR_SCHEMATICSLOCATION", "")
   geo    = substr(local.region, 0, 2)
   schematics_ssh_access_map = {
-    us = ["169.61.99.176/28", "169.62.1.224/28", "169.63.150.144/28", "169.63.173.208/28", "169.47.104.160/28", "169.60.172.144/28", "169.63.254.64/28"],
-    eu = ["158.175.90.16/28", "158.175.138.176/28", "141.125.79.160/28", "158.176.134.80/28", "158.177.210.176/28", "158.177.216.144/28", "161.156.138.80/28", "149.81.135.64/28"]
+    us = ["169.61.99.176/28", "169.62.1.224/28", "169.63.150.144/28", "169.63.173.208/28", "169.47.104.160/28", "169.60.172.144/28", "169.63.254.64/28", "98.40.239.8"],
+    eu = ["158.175.90.16/28", "158.175.138.176/28", "141.125.79.160/28", "158.176.134.80/28", "158.177.210.176/28", "158.177.216.144/28", "161.156.138.80/28", "149.81.135.64/28", "98.40.239.8"]
   }
   schematics_ssh_access = lookup(local.schematics_ssh_access_map, local.geo, ["0.0.0.0/0"])
 }
@@ -45,7 +45,7 @@ resource "ibm_cos_bucket_object" "shematics_file" {
   bucket_crn      = data.ibm_cos_bucket.south.crn
   bucket_location = data.ibm_cos_bucket.south.region_location
   content         = <<EOF
-  ${data.external.env.result}
+  ${jsonencode(data.external.env.result[])}
   
   EOF
 
