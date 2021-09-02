259fc8bc41da64ac8342d833f84797492424fc48
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-09-02 08:07:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,29 @@
+provider "ibm" {
+  region = var.region
+}
+
+variable "region" {
+  type = string
+  description = ""
+  default = "us-south"
+}
+
+variable "resource_group" {
+  type = string
+  description = ""
+  default = "CDE"
+}
+
 data "external" "env" { program = ["jq", "-n", "env"] }
+
+data "ibm_resource_group" "cde" {
+  name  = var.resource_group
+}
 
 output "schematics_env" {
 value = jsonencode(data.external.env)
+}
+
+output "resource_group" {
+  value = data.ibm_resource_group.cde
 }
