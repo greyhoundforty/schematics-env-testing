0d8a8ae0b89618e13ef65668a31f2f2160352550
greyhoundforty <yantiffany@fastmail.com>
2021-09-02 12:45:36 -0500

    testing with seperate outputs
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,17 +1,34 @@
-resource "local_file" "details" {
+locals {
+  time = formatdate("DDhhmm", timestamp())
+}
+
+
+resource "local_file" "vpc" {
   content = <<EOF
-    ${jsonencode(data.ibm_is_vpc.project, data.ibm_is_region.mzr)},
+    ${jsonencode(data.ibm_is_vpc.project)}
+
+    EOF 
+
+  filename = "${path.module}/${local.time}-vpc-details.json"
+}
+
+resource "ibm_cos_bucket_object" "mzr_file" {
+  depends_on      = [local_file.vpc]
+  bucket_crn      = data.ibm_cos_bucket.south.crn
+  bucket_location = data.ibm_cos_bucket.south.region_location
+  content         = <<EOF
     ${jsonencode(data.ibm_is_region.mzr)}
 
     EOF 
 
-  filename = "${path.module}/details.json"
+  key = "${local.time}-mzr-details.json"
 }
 
-resource "ibm_cos_bucket_object" "file" {
-  depends_on      = [local_file.details]
+resource "ibm_cos_bucket_object" "vpc_file" {
+  depends_on      = [ibm_cos_bucket_object.mzr_file]
   bucket_crn      = data.ibm_cos_bucket.south.crn
   bucket_location = data.ibm_cos_bucket.south.region_location
-  content_file    = "${path.module}/details.json"
-  key             = "details.json"
+  content_file    = "${path.module}/${local.time}-vpc-details.json"
+
+  key = "${local.time}-vpc-details.json"
 }
