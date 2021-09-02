65950e15de2a34e44a874f0acaa766695cf84718
greyhoundforty <yantiffany@fastmail.com>
2021-09-02 11:27:06 -0500

    Testing output to file
    Signed-off-by: greyhoundforty <yantiffany@fastmail.com>

--- data.tf
+++ data.tf
@@ -19,3 +19,16 @@ data "ibm_is_zones" "mzr" {
 data "ibm_is_region" "mzr" {
   name = var.region
 }
+
+data "ibm_resource_instance" "cos_instance" {
+  name              = "cdetesting-cos"
+  resource_group_id = data.ibm_resource_group.project.id
+  service           = "cloud-object-storage"
+}
+
+data "ibm_cos_bucket" "south" {
+  bucket_name          = "south-lab-1-schematics-workspace-storage"
+  resource_instance_id = data.ibm_resource_instance.cos_instance.id
+  bucket_type          = "region_location"
+  bucket_region        = "us-south"
+}
---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "local_file" "details" {
+  content = <<EOF
+    data.ibm_is_vpc.project
+    data.ibm_is_region.mzr
+
+    EOF 
+
+  filename = "${path.module}/details.json"
+}
+
+resource "ibm_cos_bucket_object" "file" {
+  depends_on      = [local_file.details]
+  bucket_crn      = data.ibm_cos_bucket.south.crn
+  bucket_location = data.ibm_cos_bucket.south.region_location
+  content_file    = "${path.module}/details.json"
+  key             = "details.json"
+}
